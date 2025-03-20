bcacf355db7d321d6be119a17e6148389ed6b7fd
PERNOT Thomas <thomas.pernot@edu.esiee.fr>
2025-03-20 10:02:39 -0400

    Print string + thread + Mutex : ne fonctionne pas correctement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-//Test de Vianney, j'espere que ca marche cette fois bachi-bouzouk de mille sabords
-
-use crossterm::{cursor, execute, terminal, ExecutableCommand};
-use std::io::{stdout, Write};
+use crossterm::{cursor, terminal, ExecutableCommand};
+use std::io::Write;
 use std::thread::sleep;
 use std::time::Duration;
 use std::io::Stdout;
 use std::str::FromStr;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 // Enumération permettant de savoir dans quelle direction les caractères de la digital rain "tomberont" 
 enum Direction 
@@ -38,8 +38,9 @@ impl FromStr for Direction
 
 
 //Affiche un caractère qui descend de (x, y_init) à (x, y_end)
-fn print_char_down(ch: char, (y_init, y_end) : (u16, u16), x: u16, mut stdout : &mut Stdout)
+fn print_char_down(ch: char, (y_init, y_end) : (u16, u16), x: u16, stdout :  Arc<Mutex<Stdout>>)
 {
+    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
 	for y in y_init..y_end //Déplacement limité à la hauteur du terminal
 	{
         stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
@@ -53,8 +54,9 @@ fn print_char_down(ch: char, (y_init, y_end) : (u16, u16), x: u16, mut stdout : 
 }
 
 //Affiche un caractère qui monte de (x, y_init) à (x, y_end)
-fn print_char_up(ch: char, (y_init, y_end) : (u16, u16), x: u16, mut stdout : &mut Stdout)
+fn print_char_up(ch: char, (y_init, y_end) : (u16, u16), x: u16, stdout :  Arc<Mutex<Stdout>>)
 {
+    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
 	for y in (y_end..y_init).rev() //Déplacement limité à la hauteur du terminal
 	{
         stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
@@ -68,8 +70,9 @@ fn print_char_up(ch: char, (y_init, y_end) : (u16, u16), x: u16, mut stdout : &m
 }
 
 //Affiche un caractère qui part de (x_init, y) à (x_end, y)
-fn print_char_to_right(ch: char, (x_init, x_end) : (u16, u16), y : u16, mut stdout : &mut Stdout)
+fn print_char_to_right(ch: char, (x_init, x_end) : (u16, u16), y : u16, stdout :  Arc<Mutex<Stdout>>)
 {
+    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
 	for x in x_init..x_end //Déplacement limité à la largeur du terminal
 	{
         stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
@@ -83,8 +86,9 @@ fn print_char_to_right(ch: char, (x_init, x_end) : (u16, u16), y : u16, mut stdo
 }
 
 //Affiche un caractère qui part de (x_init, y) à (x_end, y)
-fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16, mut stdout : &mut Stdout)
+fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16, stdout :  Arc<Mutex<Stdout>>)
 {
+    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
 	for x in (x_end..x_init).rev() //Déplacement limité à la largeur du terminal
 	{
         stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
@@ -102,7 +106,7 @@ fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16, mut stdou
 // "up" : de (x, height) à (x, y) selon l'axe x
 // "left" : de (width, y) à (x, y) selon l'axe y 
 // "right" : de (x, y) à (width, y) selon l'axe y
-fn print_char(ch: char, x : u16, y : u16, direction : &str, stdout : &mut Stdout, height : u16, width : u16)
+fn print_char(ch: char, x : u16, y : u16, direction : &str, stdout : Arc<Mutex<Stdout>>, height : u16, width : u16)
 {
     // Appel du trait FromStr de Direction 
     // direction.parse::<Direction>() = Direction::from_str(direction)
@@ -120,20 +124,37 @@ fn print_char(ch: char, x : u16, y : u16, direction : &str, stdout : &mut Stdout
     }
 }
 
-fn print_string(string: &str, x : u16, y : u16, direction : &str, stdout : &mut Stdout, height : u16, width : u16)
+fn print_string(string: &str, x : u16, y : u16, direction : &str, stdout :  Arc<Mutex<Stdout>>, height : u16, width : u16)
 {
-    for ch in string.chars()
-    {
-        print_char(ch, x, y, direction,stdout, height, width);
+    let mut handles = vec![]; //Pour stocker les handles des thread
+
+    for (i, ch) in string.chars().enumerate() {
+        let i = i as u16;
+        let stdout_clone = Arc::clone(&stdout);
+        let direction_clone = direction.to_string();
+
+        let handle = thread::spawn(move || {
+            print_char(ch, x + i, y, &direction_clone, stdout_clone, height, width);
+            //println!("Thread lancé pour {} à ({}, {})", ch, x, y);
+        });
+
+        handles.push(handle); // Stocke le handle du thread en cours
+        sleep(Duration::from_millis(10)); // Petit délai pour forcer l'ordre
     }
+
+    // Attendre la fin de tous les threads
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
 }
 
 fn main() 
 {
-    let mut stdout = stdout(); // On récupère la sortie du terminal
+    let stdout = Arc::new(Mutex::new(std::io::stdout())); // stdout sécurisé avec Mutex
     let (width, height) = terminal::size().unwrap(); // On récupère la hauteur et la largeur du terminal
 
-    print_string("abcdef", 5, 5, "down", &mut stdout, height, width);
+    print_string("abcdef", 5, 5, "down", stdout.clone(), height, width);
     //print_char('t', 10, 10, "right", &mut stdout, height, width);
 
 	//print_char_down('a', (0, height), 10, &mut stdout, height);    
