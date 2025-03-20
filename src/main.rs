68aa87626614eb1a4ae57f92e67d1b7282df1633
PERNOT Thomas <thomas.pernot@edu.esiee.fr>
2025-03-20 10:23:06 -0400

    Print string OK, affichage saccadé

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use crossterm::{cursor, terminal, ExecutableCommand};
-use std::io::Write;
+use std::io::{Write, stdout};
 use std::thread::sleep;
 use std::time::Duration;
-use std::io::Stdout;
 use std::str::FromStr;
 use std::thread;
-use std::sync::{Arc, Mutex};
 
 // Enumération permettant de savoir dans quelle direction les caractères de la digital rain "tomberont" 
 enum Direction 
@@ -38,66 +36,101 @@ impl FromStr for Direction
 
 
 //Affiche un caractère qui descend de (x, y_init) à (x, y_end)
-fn print_char_down(ch: char, (y_init, y_end) : (u16, u16), x: u16, stdout :  Arc<Mutex<Stdout>>)
+fn print_char_down(ch: char, (y_init, y_end) : (u16, u16), x: u16)
 {
-    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
+    let mut stdout = stdout(); // stdout exclusif à la fonction
 	for y in y_init..y_end //Déplacement limité à la hauteur du terminal
 	{
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
-        print!("{ch}"); // Affiche un caractère
-        stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
+            print!("{ch}"); // Affiche un caractère
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+        
         sleep(Duration::from_millis(100)); // Pause pour l'animation
 
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
-        print!(" "); // Remplace le caractère par "" : le caractère est effacé
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
+            print!(" "); // Remplace le caractère par "" : le caractère est effacé
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }  
     }
 }
 
 //Affiche un caractère qui monte de (x, y_init) à (x, y_end)
-fn print_char_up(ch: char, (y_init, y_end) : (u16, u16), x: u16, stdout :  Arc<Mutex<Stdout>>)
+fn print_char_up(ch: char, (y_init, y_end) : (u16, u16), x: u16)
 {
-    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
+    let mut stdout = stdout(); // stdout exclusif à la fonction
 	for y in (y_end..y_init).rev() //Déplacement limité à la hauteur du terminal
 	{
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
-        print!("{ch}"); // Affiche un caractère
-        stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
+            print!("{ch}"); // Affiche un caractère
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+        
         sleep(Duration::from_millis(100)); // Pause pour l'animation
 
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
-        print!(" "); // Remplace le caractère par "" : le caractère est effacé
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
+            print!(" "); // Remplace le caractère par "" : le caractère est effacé
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+        
     }
 }
 
 //Affiche un caractère qui part de (x_init, y) à (x_end, y)
-fn print_char_to_right(ch: char, (x_init, x_end) : (u16, u16), y : u16, stdout :  Arc<Mutex<Stdout>>)
+fn print_char_to_right(ch: char, (x_init, x_end) : (u16, u16), y : u16)
 {
-    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
+    let mut stdout = stdout(); // stdout exclusif à la fonction
 	for x in x_init..x_end //Déplacement limité à la largeur du terminal
 	{
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
-        print!("{ch}"); // Affiche un caractère
-        stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
+            print!("{ch}"); // Affiche un caractère
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+
         sleep(Duration::from_millis(100)); // Pause pour l'animation
 
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
-        print!(" "); // Remplace le caractère par "" : le caractère est effacé
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
+            print!(" "); // Remplace le caractère par "" : le caractère est effacé
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+
     }
 }
 
 //Affiche un caractère qui part de (x_init, y) à (x_end, y)
-fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16, stdout :  Arc<Mutex<Stdout>>)
+fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16)
 {
-    let mut stdout = stdout.lock().unwrap(); // Vérouillage de stdout
+    let mut stdout = stdout(); // stdout exclusif à la fonction
 	for x in (x_end..x_init).rev() //Déplacement limité à la largeur du terminal
 	{
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
-        print!("{ch}"); // Affiche un caractère
-        stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Déplace le curseur
+            print!("{ch}"); // Affiche un caractère
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+ 
         sleep(Duration::from_millis(100)); // Pause pour l'animation
 
-        stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
-        print!(" "); // Remplace le caractère par "" : le caractère est effacé
+        {
+            let _lock = stdout.lock(); // Pour forcer l'ordre d'affichage
+            stdout.execute(cursor::MoveTo(x, y)).unwrap(); // Place le curseur au même endroit que le caractère
+            print!(" "); // Remplace le caractère par "" : le caractère est effacé
+            stdout.flush().unwrap(); // Force l'affichage immédiat du caractère
+        }
+
     }
 }
 
@@ -106,16 +139,16 @@ fn print_char_to_left(ch: char, (x_init, x_end) : (u16, u16), y : u16, stdout : 
 // "up" : de (x, height) à (x, y) selon l'axe x
 // "left" : de (width, y) à (x, y) selon l'axe y 
 // "right" : de (x, y) à (width, y) selon l'axe y
-fn print_char(ch: char, x : u16, y : u16, direction : &str, stdout : Arc<Mutex<Stdout>>, height : u16, width : u16)
+fn print_char(ch: char, x : u16, y : u16, direction : &str, height : u16, width : u16)
 {
     // Appel du trait FromStr de Direction 
     // direction.parse::<Direction>() = Direction::from_str(direction)
 	match Direction::from_str(direction) // On match la valeur du type Result
     {
-        Ok(Direction::Down) => print_char_down(ch, (y, height), x, stdout),
-        Ok(Direction::Up) => print_char_up(ch, (height, y), x, stdout),
-        Ok(Direction::Left) => print_char_to_left(ch, (width, x), y, stdout),
-        Ok(Direction::Right) => print_char_to_right(ch, (x, width), y, stdout),
+        Ok(Direction::Down) => print_char_down(ch, (y, height), x),
+        Ok(Direction::Up) => print_char_up(ch, (height, y), x),
+        Ok(Direction::Left) => print_char_to_left(ch, (width, x), y,),
+        Ok(Direction::Right) => print_char_to_right(ch, (x, width), y),
         Err(err) => 
         {
             eprintln!("{}", err);
@@ -124,17 +157,16 @@ fn print_char(ch: char, x : u16, y : u16, direction : &str, stdout : Arc<Mutex<S
     }
 }
 
-fn print_string(string: &str, x : u16, y : u16, direction : &str, stdout :  Arc<Mutex<Stdout>>, height : u16, width : u16)
+fn print_string(string: &str, x : u16, y : u16, direction : &str, height : u16, width : u16)
 {
     let mut handles = vec![]; //Pour stocker les handles des thread
 
     for (i, ch) in string.chars().enumerate() {
         let i = i as u16;
-        let stdout_clone = Arc::clone(&stdout);
         let direction_clone = direction.to_string();
 
         let handle = thread::spawn(move || {
-            print_char(ch, x + i, y, &direction_clone, stdout_clone, height, width);
+            print_char(ch, x + i, y, &direction_clone, height, width);
             //println!("Thread lancé pour {} à ({}, {})", ch, x, y);
         });
 
@@ -151,10 +183,9 @@ fn print_string(string: &str, x : u16, y : u16, direction : &str, stdout :  Arc<
 
 fn main() 
 {
-    let stdout = Arc::new(Mutex::new(std::io::stdout())); // stdout sécurisé avec Mutex
     let (width, height) = terminal::size().unwrap(); // On récupère la hauteur et la largeur du terminal
 
-    print_string("abcdef", 5, 5, "down", stdout.clone(), height, width);
+    print_string("abcdef", 5, 5, "down", height, width);
     //print_char('t', 10, 10, "right", &mut stdout, height, width);
 
 	//print_char_down('a', (0, height), 10, &mut stdout, height);    
